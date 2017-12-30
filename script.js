289794b9b3250d64239ebe49d0992cee6e07590b
smanzar <smanzar95@gmail.com>
2017-12-29 18:26:36 -0800

    responsive going

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready (function(){
+    $('.mar .ab').on('click',function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+    $('.btns').on('click',function(){
+        var btnv=this.name;
+        $("#"+btnv).fadeIn(800).show().siblings().fadeOut(950).hide();
+    });
+    $('.slide').on('click',function(){
+        var slid=this.name;
+        //console.log(slid);
+       $("#"+slid).fadeIn(800).show().siblings().fadeOut(950).hide();
+        if(slid == "show2"){
+            $(".fa-chevron-right").addClass("disable");
+        }else if(slid == "show1"){
+            $(".fa-chevron-left").addClass("disable");
+        }
+    });
+});
+
+
